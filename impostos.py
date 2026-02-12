bcd6cf57baf30d5c134c3ea5eb87d2d02af2784c
correahenrique <correahenrique378@gmail.com>
2026-02-12 00:09:32 -0300

    atividade 18: impostos

--- impostos.py
+++ impostos.py
@@ -4,4 +4,4 @@ distribuidor = fabrica * 0.28
 imposto = fabrica * 0.45
 valor = fabrica + distribuidor + imposto
 
-print(f"O valor do carro era {fabrica} na fábrica, no final ficou {valor} com os impostos e distribuidores")
+print(f"O valor do carro era {fabrica} na fábrica, no final ficou {valor:.2f} com os impostos e distribuidores")
