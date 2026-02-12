d52d2a09e39327d28e3a4f674a3414384c3dcd0f
correahenrique <correahenrique378@gmail.com>
2026-02-11 23:28:00 -0300

    atividade 10: multiplos

---
+++ multiplos.py
@@ -0,0 +1,8 @@
+print("São múltiplos?")
+a = float(input("Digite o primeiro valor: "))
+b = float(input("Digite o segundo valor: "))
+
+if a % b == 0:
+    print("São múltiplos!")
+else:
+    print("Não são múltiplos!")
