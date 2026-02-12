3155e623f9b34b0161d91484bc56923d938c67c5
correahenrique <correahenrique378@gmail.com>
2026-02-11 23:41:24 -0300

    atividade 13: tempoViagem

---
+++ tempoViagem.py
@@ -0,0 +1,6 @@
+print("Vamos calcular o tempo da sua viagem")
+distancia = float(input("Qual a distância da viagem? "))
+velocidade = float(input("Qual a velocidade média que você está? "))
+tempo = distancia / velocidade
+
+print(f"O tempo q vc vai demorar pra chegar é: {tempo:.2f} horas")
