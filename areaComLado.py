07e75d3bd3def89da0e7c27116c9194462b3c856
correahenrique <correahenrique378@gmail.com>
2026-02-11 23:00:26 -0300

    atividade 5: areaComLado

---
+++ areaComLado.py
@@ -0,0 +1,5 @@
+print("Vamos descobrir a área do quadrado")
+lado = float(input("Digite o valor de um lado do quadrado: "))
+area = lado ** 2
+
+print(f"O quadrado com lados do valor de {lado} tem área equivalente a {area}")
