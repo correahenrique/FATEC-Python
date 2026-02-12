f8f16a81926cf0627bbac2dac097cccdb384807e
correahenrique <correahenrique378@gmail.com>
2026-02-11 23:31:27 -0300

    atividade 11: desconto

---
+++ desconto.py
@@ -0,0 +1,5 @@
+print("Bem vindo ao mercadinho, você vai ganhar um desconto de 10%!!!")
+valor = float(input("Qual o valor do produto?"))
+desconto = valor * 0.9
+
+print(f"O valor era {valor} e ficou {desconto} com o desconto de 10%!!!!!!!")
