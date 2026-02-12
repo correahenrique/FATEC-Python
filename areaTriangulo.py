5e12821151a331015aa03e3200a81d6a14eb5349
correahenrique <correahenrique378@gmail.com>
2026-02-11 23:05:08 -0300

    atividade 6: areaTriangulo

---
+++ areaTriangulo.py
@@ -0,0 +1,6 @@
+print("Vamos descobrir a área do triângulo")
+base = float(input("Digite o valor da base do triângulo:"))
+altura = float(input("Digite o valor da altura do triângulo:"))
+area = (base * altura) / 2
+
+print(f"A área do triângulo é: {area}")
