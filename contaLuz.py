33aabb2086aee0969e09ac23c6cbc4d32eed9992
correahenrique <correahenrique378@gmail.com>
2026-02-11 23:45:48 -0300

    atividade 14: contaLuz

---
+++ contaLuz.py
@@ -0,0 +1,6 @@
+print("Vamos calcular sua conta de luz")
+quantos = float(input("Quantos kWh você consumiu?"))
+kWh = float(input("Qual o valor do kWh?"))
+valor = kWh * quantos
+
+print(f"Ficou {valor} reais sua conta.")
