f94a65cadb2a6de8784ba812c7787b1dedf2f63c
correahenrique <correahenrique378@gmail.com>
2026-02-11 22:54:53 -0300

    atividade 4: calculoArea1

---
+++ calculoArea1.py
@@ -0,0 +1,6 @@
+print("Vamos descobrir a área")
+base = float(input("Digite o valor da base: "))
+altura = float(input("Digite o valor da altura: "))
+area = base * altura
+
+print(f"A área é: {area}")
