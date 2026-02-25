6908212f2f5f2d94916f8e23493a32abadc19141
correahenrique <correahenrique378@gmail.com>
2026-02-25 11:11:42 -0300

    Lista num2
    aula do wellington

---
+++ idade2.py
@@ -0,0 +1,8 @@
+idade = int(input("Qual é a sua idade? "))
+
+if (idade < 18):
+    print("Você é menor de idade!")
+elif (idade < 65):
+    print("Você é maior de idade!")
+else:
+    print("Você é idoso!")
---
+++ nota7.py
@@ -0,0 +1,12 @@
+nota = int(input("Digite sua nota (0 a 100): "))
+
+if (nota > 89):
+    print("A sua nota foi: A")
+elif (nota > 79):
+    print("A sua nota foi: B")
+elif (nota > 69):
+    print("A sua nota foi: C")
+elif (nota > 59):
+    print("A sua nota foi: D")
+else:
+    print("Sua nota foi F")
