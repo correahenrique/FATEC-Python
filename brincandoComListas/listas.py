7cdebf97f51248efdcb7cd4e12346bbc44b3c2a5
correahenrique <correahenrique378@gmail.com>
2026-03-04 17:55:10 -0300

    Update listas.py

--- brincandoComListas/listas.py
+++ brincandoComListas/listas.py
@@ -1,12 +1,13 @@
-emails = ["henrique@gmail.com", "diretor@vilt.com",
-          "contato@outlook.com", "suporte@vilt.com", "rh@yahoo.com"]
+emails = ["henrique@gmail.com", "diretor@hotmail.com",
+          "contato@outlook.com", "suporte@hotmail.com", "rh@yahoo.com"]
 i = 0
 while i < len(emails):
     lista = emails[i]
     separar = lista.split("@")
 
-    if not lista.endswith("@vilt.com"):
+    if not lista.endswith("@hotmail.com"):
         emails[i] = separar[0] + "@CONFIDENCIAL"
 
     i += 1
+
 print(emails)
