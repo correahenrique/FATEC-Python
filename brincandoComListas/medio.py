41f8644dbf822d5c448f3181fe1861cf86be512b
correahenrique <correahenrique378@gmail.com>
2026-03-04 17:55:40 -0300

    Update medio.py

--- brincandoComListas/medio.py
+++ brincandoComListas/medio.py
@@ -2,7 +2,7 @@ arquivos = [
     {"titulo": "Contrato_2018", "ano": 2018},
     {"titulo": "Relatorio_2024", "ano": 2024},
     {"titulo": "Nota_Fiscal_2015", "ano": 2015},
-    {"titulo": "Projeto_VILT_2026", "ano": 2026}
+    {"titulo": "Projeto_2026", "ano": 2026}
 ]
 para_arquivo_morto = []
 
@@ -10,5 +10,6 @@ for arquivo in arquivos:
     nome = arquivo["titulo"]
     if arquivo["ano"] < 2020:
         para_arquivo_morto.append(nome)
+
 
 print(para_arquivo_morto)
